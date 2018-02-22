13c30caf32e76ceda596f279d9c1b5601836de8d
RichardPet <36713563+RichardPet@users.noreply.github.com>
2018-02-22 18:31:32 +0100

    Update Form1.vb

--- WinVBApp/WinVBApp/Form1.vb
+++ WinVBApp/WinVBApp/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Dette er kommentar legt til på GitHub
         MsgBox("Hello...")
     End Sub
 End Class
